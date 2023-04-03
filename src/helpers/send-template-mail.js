3729e9bd310ed3ad07faf63c260b2b9f927ce3fb
code-flag <awefrancolaz@gmail.com>
2023-04-03 10:07:34 +0100

    debug: unused code commented out

--- src/helpers/send-template-mail.js
+++ src/helpers/send-template-mail.js
@@ -1,45 +1,45 @@
-import hbs from "nodemailer-express-handlebars";
-import path from "path";
-import { createTransport } from "nodemailer";
-import { config } from "dotenv";
-config();
+// import hbs from "nodemailer-express-handlebars";
+// import path from "path";
+// import { createTransport } from "nodemailer";
+// import { config } from "dotenv";
+// config();
 
-export const sendTemplateEmail = async ({ to, subject, template, context }) => {
+// export const sendTemplateEmail = async ({ to, subject, template, context }) => {
   
-  const transporter = createTransport({
-    port: process.env.EMAIL_PORT,
-    host: process.env.EMAIL_HOST,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    }
-  });
+//   const transporter = createTransport({
+//     port: process.env.EMAIL_PORT,
+//     host: process.env.EMAIL_HOST,
+//     secure: true,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     }
+//   });
 
-  const handlebarOptions = {
-    viewEngine: {
-      extName: ".handlebars",
-      pathDir: path.resolve("src/views"),
-      defaultLayout: false,
-    },
-    viewPath: path.resolve("src/views"),
-    extName: ".handlebars",
-  };
+//   const handlebarOptions = {
+//     viewEngine: {
+//       extName: ".handlebars",
+//       pathDir: path.resolve("src/views"),
+//       defaultLayout: false,
+//     },
+//     viewPath: path.resolve("src/views"),
+//     extName: ".handlebars",
+//   };
 
-  transporter.use("compile", hbs(handlebarOptions));
-  return transporter.sendMail(
-    {
-      from: process.env.EMAIL_USER, // sender address
-      to, // list of receivers
-      subject, // Subject line
-      template,
-      context,
-    },
-    (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log(`Message sent to ${to} successfully`);
-    }
-  );
-};
+//   transporter.use("compile", hbs(handlebarOptions));
+//   return transporter.sendMail(
+//     {
+//       from: process.env.EMAIL_USER, // sender address
+//       to, // list of receivers
+//       subject, // Subject line
+//       template,
+//       context,
+//     },
+//     (error, info) => {
+//       if (error) {
+//         return console.log(error);
+//       }
+//       console.log(`Message sent to ${to} successfully`);
+//     }
+//   );
+// };
